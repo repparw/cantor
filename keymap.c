d2be995d5480a61aa001fd4fe1b3cacba831e612
repparw <ubritos@gmail.com>
2025-04-10 22:22:26 -0300

    name layers

--- keymap.c
+++ keymap.c
@@ -1,4 +1,3 @@
-
 // Copyright 2025 Ulises Britos (@repparw)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
@@ -21,30 +20,37 @@
 #define SFT_SPC LSFT_T(KC_SPC)
 #define CTL_ESC LCTL_T(KC_ESC)
 
+enum layer_names {
+    _BASE,
+    _NUM,
+    _FUNC,
+    _GAME
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-        CTL_ESC, LGUI_A,  LALT_S,  LCTL_D,  LSFT_F,   KC_G,      KC_H,   RSFT_J,  RCTL_K,  RALT_L,  RGUI_SC,  KC_QUOT,
-        OSM_LSFT,KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                          OSM_LGUI, OSL(1),  SFT_SPC,             KC_ENT, OSL(2),  OSM_RALT
+    [_BASE] = LAYOUT_split_3x6_3(
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,    KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+        CTL_ESC,  LGUI_A,   LALT_S,   LCTL_D,   LSFT_F,   KC_G,      KC_H,    RSFT_J,   RCTL_K,   RALT_L,   RGUI_SC,  KC_QUOT,
+        OSM_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  OSM_RALT,
+                            XXXXXXXX,  OSL(1),   SFT_SPC,             KC_ENT,  OSL(2),   OSM_RALT
     ),
-    [1] = LAYOUT_split_3x6_3(
-        KC_GRV,  KC_NO,   KC_7,    KC_8,    KC_9,     KC_COMM,   KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_NO,    KC_BSPC,
-        KC_TRNS, KC_NO,   KC_4,    KC_5,    KC_6,     KC_DOT,    KC_PMNS, KC_PEQL, KC_LBRC, KC_RBRC, KC_NO,    KC_BSLS,
-        KC_TRNS, KC_NO,   KC_1,    KC_2,    KC_3,     KC_0,      KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_NO,    KC_TRNS,
-                          KC_TRNS, KC_TRNS,  KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
+    [_NUM] = LAYOUT_split_3x6_3(
+        KC_GRV,   XXXXXXXX, KC_7,     KC_8,     KC_9,     KC_COMM,   XXXXXXXX, XXXXXXXX, KC_LPRN,  KC_RPRN,  XXXXXXXX, KC_BSPC,
+        ________, XXXXXXXX, KC_4,     KC_5,     KC_6,     KC_DOT,    KC_PMNS,  KC_PEQL,  KC_LBRC,  KC_RBRC,  XXXXXXXX, KC_BSLS,
+        ________, XXXXXXXX, KC_1,     KC_2,     KC_3,     KC_0,      KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,  XXXXXXXX, ________,
+                            ________, ________,  ________,             ________, ________, ________
     ),
-    [2] = LAYOUT_split_3x6_3(
-        KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,    KC_F12,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_PSCR,  KC_DEL,
-        KC_TRNS, KC_NO,   KC_F4,   KC_F5,   KC_F6,    KC_F11,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,    KC_NO,
-        KC_TRNS, KC_NO,   KC_F1,   KC_F2,   KC_F3,    KC_F10,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    TG(3),
-                          KC_TRNS, KC_TRNS,  KC_TRNS,             KC_TRNS, KC_TRNS, KC_TRNS
+    [_FUNC] = LAYOUT_split_3x6_3(
+        XXXXXXXX, XXXXXXXX, KC_F7,    KC_F8,    KC_F9,    KC_F12,    KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   KC_PSCR,  KC_DEL,
+        ________, XXXXXXXX, KC_F4,    KC_F5,    KC_F6,    KC_F11,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXXX, XXXXXXXX,
+        ________, XXXXXXXX, KC_F1,    KC_F2,    KC_F3,    KC_F10,    XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, TG(3),
+                            ________, ________,  ________,             ________, ________, ________
     ),
-    [3] = LAYOUT_split_3x6_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_TRNS,
-                          KC_ESC,  KC_TRNS,  KC_SPC,              KC_ENT,  KC_TRNS, KC_LALT
+    [_GAME] = LAYOUT_split_3x6_3(
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ________,
+                            KC_ESC,   ________,  KC_SPC,              KC_ENT,   ________, KC_LALT
     )
 };
 
@@ -57,4 +63,3 @@ __attribute__((weak)) const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] P
                           'L',     'L',      'L',                  'R',     'R',     'R'
     );
 #endif
-
