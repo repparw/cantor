1f59006c127e107c5281373c52cebdc880e3ff50
repparw <ubritos@gmail.com>
2025-04-10 22:05:22 -0300

    back to default tap hold

--- keymap.c
+++ keymap.c
@@ -1,3 +1,4 @@
+
 // Copyright 2025 Ulises Britos (@repparw)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
@@ -7,10 +8,10 @@
 // Home row mods aliases
 #define LGUI_A LGUI_T(KC_A)
 #define LALT_S LALT_T(KC_S)
-#define LSFT_D LSFT_T(KC_D)
-#define LCTL_F LCTL_T(KC_F)
-#define RCTL_J RCTL_T(KC_J)
-#define RSFT_K RSFT_T(KC_K)
+#define LCTL_D LCTL_T(KC_D)
+#define LSFT_F LSFT_T(KC_F)
+#define RSFT_J RSFT_T(KC_J)
+#define RCTL_K RCTL_T(KC_K)
 #define RALT_L RALT_T(KC_L)
 #define RGUI_SC RGUI_T(KC_SCLN)
 
@@ -23,7 +24,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,      KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-        CTL_ESC, LGUI_A,  LALT_S,  LSFT_D,  LCTL_F,   KC_G,      KC_H,   RCTL_J,  RSFT_K,  RALT_L,  RGUI_SC,  KC_QUOT,
+        CTL_ESC, LGUI_A,  LALT_S,  LCTL_D,  LSFT_F,   KC_G,      KC_H,   RSFT_J,  RCTL_K,  RALT_L,  RGUI_SC,  KC_QUOT,
         OSM_LSFT,KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,      KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                           OSM_LGUI, OSL(1),  SFT_SPC,             KC_ENT, OSL(2),  OSM_RALT
     ),
