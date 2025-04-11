f56ef6f92343bb9b12cf6814fa3c4e67ee6bab1d
repparw <ubritos@gmail.com>
2025-04-10 23:23:01 -0300

    gaming layer transparent on base

--- keymap.c
+++ keymap.c
@@ -34,6 +34,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         OSM_SFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  OSM_RALT,
                             XXXXXXXX, OSL(_NL),  SFT_SPC,              KC_ENT,  OSL(_FL), OSM_RALT
     ),
+    [_GL] = LAYOUT_split_3x6_3(
+        ________, ________, ________, ________, ________, ________,  ________, ________, ________, ________, ________, ________,
+        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      ________, KC_J,     KC_K,     KC_L,     KC_SCLN,  ________,
+        KC_LSFT,  ________, ________, ________, ________, ________,  ________, ________, ________, ________, ________, ________,
+                               KC_ESC,   ________, KC_SPC,              ________, ________, KC_LALT
+    ),
     [_NL] = LAYOUT_split_3x6_3(
         KC_GRV,   KC_T, KC_7,     KC_8,     KC_9,     KC_COMM,   XXXXXXXX, XXXXXXXX, KC_LPRN,  KC_RPRN,  XXXXXXXX, KC_BSPC,
         ________, XXXXXXXX, KC_4,     KC_5,     KC_6,     KC_DOT,    KC_PMNS,  KC_PEQL,  KC_LBRC,  KC_RBRC,  XXXXXXXX, KC_BSLS,
@@ -45,12 +51,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ________, XXXXXXXX, KC_F4,    KC_F5,    KC_F6,    KC_F11,    KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XXXXXXXX, XXXXXXXX,
         ________, XXXXXXXX, KC_F1,    KC_F2,    KC_F3,    KC_F10,    XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, XXXXXXXX, TG(3),
                             ________, ________,  ________,             ________, ________, ________
-    ),
-    [_GL] = LAYOUT_split_3x6_3(
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  ________,
-                            KC_ESC,   ________,  KC_SPC,              KC_ENT,   ________, KC_LALT
     )
 };
 
