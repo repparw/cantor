b8f5af95293fff5b07250fbb4939b870bbbd743b
repparw <ubritos@gmail.com>
2025-04-11 21:41:55 -0300

    enable caps word

--- keyboards/cantor/keymaps/repparw/keymap.c
+++ keyboards/cantor/keymaps/repparw/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_split_3x6_3(
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,     KC_O,     KC_P,    KC_BSPC,
         XXXXXXX,  GUI_A,    ALT_S,    CTL_D,    SFT_F,    KC_G,    KC_H,    SFT_J,    CTL_K,    ALT_L,    GUI_SC,  KC_QUOT,
-        OSM_SFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH, OSM_RALT,
+        CW_TOGG,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,   KC_SLSH, OSM_RALT,
                             QK_GESC,  NL_SPC,   KC_TAB,             KC_ENT,  NL_BSPC,  FL_DEL
     ),
     [_NL] = LAYOUT_split_3x6_3(
